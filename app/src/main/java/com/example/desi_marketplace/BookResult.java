ae6229fa698adb95bde4e9a587c9b5f4c52b3881
divyanshutw <60772636+divyanshutw@users.noreply.github.com>
2020-07-26 23:27:48 +0530

    Book complete Chats fixed

--- app/src/main/java/com/example/desi_marketplace/BookResult.java
+++ app/src/main/java/com/example/desi_marketplace/BookResult.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -25,9 +24,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
-public class ChatsResult extends AppCompatActivity {
-
-    LinearLayout layout;
+public class BookResult extends AppCompatActivity {
 
     FirebaseFirestore firestore;
 
@@ -36,15 +33,15 @@ public class ChatsResult extends AppCompatActivity {
 
     String userType,Uid;
 
+    LinearLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chats_result);
+        setContentView(R.layout.activity_book_result);
         getSupportActionBar().hide();
 
         layout=findViewById(R.id.layout);
-
-        firestore=FirebaseFirestore.getInstance();
 
         preferences=getSharedPreferences("LOGIN_INFO",MODE_PRIVATE);
         editor=preferences.edit();
@@ -52,17 +49,15 @@ public class ChatsResult extends AppCompatActivity {
         userType=preferences.getString("UserType","");
         Uid=preferences.getString("Uid","");
 
-        firestore.collection("EndToEnd").document("Chats").collection(Uid).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        firestore=FirebaseFirestore.getInstance();
+
+        firestore.collection("EndToEnd").document("Book").collection(Uid).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                ArrayList<String> chats=new ArrayList<>();
-                //Toast.makeText(ChatsResult.this, "reached", Toast.LENGTH_SHORT).show();
-                for(QueryDocumentSnapshot queryDocumentSnapshot:queryDocumentSnapshots)
-                {
-                    chats.add(queryDocumentSnapshot.getId());
-                    //Toast.makeText(ChatsResult.this, queryDocumentSnapshot.getId(), Toast.LENGTH_SHORT).show();
-                }
-                show(chats);
+                ArrayList<String> books=new ArrayList<>();
+                for(QueryDocumentSnapshot documentSnapshot:queryDocumentSnapshots)
+                    books.add(documentSnapshot.getId());
+                show(books);
             }
         });
     }
@@ -71,9 +66,10 @@ public class ChatsResult extends AppCompatActivity {
     {
         if(Uids.size()==0)
         {
-            TextView textView=new TextView(ChatsResult.this);
+            TextView textView=new TextView(BookResult.this);
             textView.setText("No results");
             textView.setTextColor(Color.parseColor("#eeeeee"));
+
             textView.setGravity(Gravity.CENTER);
             layout.addView(textView);
         }
@@ -91,7 +87,7 @@ public class ChatsResult extends AppCompatActivity {
                             sellerType[0] = task.getResult().getString(Uid);
                     }
                     LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-                    final Button button=new Button(ChatsResult.this);
+                    final Button button=new Button(BookResult.this);
                     button.setId(finalI);
                     button.setBackgroundColor(Color.WHITE);
                     final String s= sellerType[0].equals("Manufacturer")?"EnterpriseName":(sellerType[0].equals("Retailer")?"ShopName":"Firstname");
@@ -112,15 +108,13 @@ public class ChatsResult extends AppCompatActivity {
                     button1.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-                            Intent goToChat=new Intent(ChatsResult.this,ChatPage.class);
-                            goToChat.putExtra("SellerUid",Uid);
-                            goToChat.putExtra("SellerType",sellerType[0]);
-                            goToChat.putExtra("Message","");
-                            startActivity(goToChat);
-
+                            Intent intent=new Intent(BookResult.this,BookPage.class);
+                            intent.putExtra("SellerType", sellerType[0]);
+                            intent.putExtra("SellerUid",Uid);
+                            startActivity(intent);
                         }
                     });
-                    TextView textView=new TextView(ChatsResult.this);
+                    TextView textView=new TextView(BookResult.this);
                     textView.setHeight(5);
                     layout.addView(textView);
                 }
